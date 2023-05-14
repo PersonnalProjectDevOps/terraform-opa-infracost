faa916637374d41ee9e59867eb06cabe33b9d455
Nasr-Ladib <nasr.ladib@esrpit.tn>
2023-05-14 18:29:46 +0200

    fix: deprecated db name resource

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_db_instance" "example" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  name                 = "example-db"
+  db_name                 = "example-db"
   username             = var.db_username
   password             = var.db_password
   parameter_group_name = "default.mysql5.7"
