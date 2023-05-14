f566eea0548a17cef024992baae6a406c366d550
NLadib-PartnerAuchan <nladib@partner.auchan.com>
2023-05-14 18:26:43 +0200

    feat: add resources

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # This is an example Amazon Linux 2 AMI ID; replace with an appropriate AMI ID for your region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+    env  = "development"
+    team = "example-team"
+  }
+}
+
+resource "aws_db_instance" "example" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "example-db"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql5.7"
+
+  tags = {
+    Name = "example-db"
+    env  = "development"
+    team = "example-team"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "db_username" {
+  description = "The database username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "The database password"
+  type        = string
+  sensitive   = true
+}
